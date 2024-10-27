49bcb10bd599f76b2dd71295af4e430697e96ae1
statelinecat <statelinecat@gmail.com>
2024-10-27 20:50:18 +0300

    add code2

---
+++ movie_project/films/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('add_film', views.add_film, name='add_film'),
+]
--- movie_project/films/views.py
+++ movie_project/films/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'films/index.html', {'title': 'Фильмы'})
+
+def add_film(request):
+    return render(request, 'films/add_film.html', {'title': 'Добавление нового фильма'})
